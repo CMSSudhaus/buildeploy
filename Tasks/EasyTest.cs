e21ce5730011343ad256819a5b3e2dd9fc9d4ab3
Sergej Derjabkin <sergej@derjabkin.de>
2016-11-07 18:41:12 +0100

    - EasyTest: Analyzing test log

--- Tasks/EasyTest.cs
+++ Tasks/EasyTest.cs
@@ -1,26 +1,54 @@
-﻿using Microsoft.Build.Utilities;
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Cms.Buildeploy.Tasks
 {
     public class EasyTest : ToolTask
     {
+        private const string logFileName = "EasyTestLog.log";
+
         protected override string ToolName => "TestExecutor";
 
-        protected override string GenerateFullPathToTool()
+        protected override string GenerateFullPathToTool() => TextExecutorPath;
+
+        [Required]
+        public string TextExecutorPath { get; set; }
+        public override bool Execute()
         {
-            return @"c:\Temp\EasyTests\TestExecutor.v16.1.exe";
+            return base.Execute() && AnalyzeLogFile();
         }
 
-        public string ScriptFile { get; set; }
+        public string ScriptsDirectory { get; set; }
 
+        private bool AnalyzeLogFile()
+        {
+            var doc = XElement.Load(LogFilePath);
+            bool result = false;
+            foreach (var failedTest in doc.Elements("Test").Where(e => e.Attribute("Result")?.Value != "Passed"))
+            {
+                result = true;
+                var errorElement = failedTest.Element("Error");
+                if (errorElement != null)
+                    Log.LogError("Test '{0}' failed: {1}, {2}",
+                        failedTest.Attribute("Name").Value,
+                        errorElement.Attribute("Type").Value, 
+                        errorElement.Element("Message").Value);
+            }
+
+            return result;
+        }
+
+        private string LogFilePath => Path.GetFullPath(Path.Combine(ScriptsDirectory, logFileName));
         protected override string GenerateCommandLineCommands()
         {
-            return $"\"{ScriptFile}\" -l:EasyTestLog.log";
+            return $"\"{ScriptsDirectory}\" -l:{LogFilePath}";
         }
     }
 }
---
+++ Tasks/SqlServerRestoreDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Cms.Buildeploy.Tasks
+{
+    public class SqlServerRestoreDatabase : Task
+    {
+
+        [Required]
+        public string ConnectionString { get; set; }
+
+
+        public override bool Execute()
+        {
+            using (var connection = new SqlConnection())
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    return true;
+                }
+            }
+
+        }
+    }
+}
