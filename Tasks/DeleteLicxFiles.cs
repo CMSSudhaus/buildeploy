d779bd2c23a3b0a2a6c70b0495f633f21b7238cb
Lashkevich Ryan <lashkevichrain@gmail.com>
2017-01-24 18:13:15 +0700

    Not tested

---
+++ Tasks/DeleteLicxFiles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Evaluation;
+
+namespace Cms.Buildeploy.Tasks
+{
+    public class DeleteLicxFiles : Task
+    {
+        public override bool Execute()
+        {
+            try
+            {
+                foreach (ITaskItem projectFile in ProjectFiles)
+                {
+                    foreach (var licx in Directory.EnumerateFiles(Path.GetDirectoryName(projectFile.ItemSpec) ?? string.Empty, "*.licx", SearchOption.AllDirectories))
+                    {
+                        File.Delete(licx);
+                    }
+
+                   var project = new Project(File.ReadAllText(projectFile.ItemSpec));
+                   var license = project.GetItems("EmbeddedResource").SingleOrDefault(x => x.EvaluatedInclude.EndsWith("licenses.licx")); 
+                   if (license != null)
+                        project.RemoveItem(license);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false; 
+            }
+        }
+
+        [Required]
+        public ITaskItem[] ProjectFiles { get; set; }
+
+    }
+}
