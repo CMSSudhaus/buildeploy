c6d606ea013471b1faecd669578ff01e326609ea
Lashkevich Ryan <lashkevichrain@gmail.com>
2017-01-27 17:40:26 +0700

    Fixed

--- Tasks/DeleteLicxFiles.cs
+++ Tasks/DeleteLicxFiles.cs
@@ -1,43 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.Build.Evaluation;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
-using Microsoft.Build.Evaluation;
+using static System.Diagnostics.Debugger;
 
 namespace Cms.Buildeploy.Tasks
 {
     public class DeleteLicxFiles : Task
     {
-        public override bool Execute()
-        {
-            try
-            {
-                foreach (ITaskItem projectFile in ProjectFiles)
-                {
-                   var project = new Project(projectFile.ItemSpec);
-                   var license = project.GetItemsByEvaluatedInclude(@"Properties\licenses.licx").SingleOrDefault();
-                   if (license != null)
-                   {
-                        project.RemoveItem(license);
-                        project.Save();
-
-                        foreach (var licx in Directory.EnumerateFiles(Path.GetDirectoryName(projectFile.ItemSpec) ?? string.Empty, "*.licx", SearchOption.AllDirectories))
-                        {
-                            File.Delete(licx);
-                        }
-                    }
-                }
-                return true;
-            }
-            catch (Exception)
-            {
-                return false; 
-            }
-        }
-
         [Required]
         public ITaskItem[] ProjectFiles { get; set; }
 
+        public ITaskItem SolutionLocation { get; set; }
+
+        public override bool Execute()
+        {
+            if (!string.IsNullOrEmpty(SolutionLocation.ItemSpec))
+            {
+                foreach (var projectLocation in GetProjectLocations(SolutionLocation.ItemSpec))
+                {
+                    HandleProjectFile(projectLocation);   
+                }
+                return true;
+            }
+            
+            foreach (var projectFile in ProjectFiles)
+            {
+                HandleProjectFile(projectFile.ItemSpec);
+            }
+            return true;
+        }
+
+        private IEnumerable<string> GetProjectLocations(string solutionLocation)
+        {
+            var solutionDirectory = Path.GetDirectoryName(solutionLocation) ?? string.Empty;
+
+            foreach (var line in File.ReadLines(solutionLocation))
+            {
+                if (line.IndexOf("Project", StringComparison.InvariantCulture) != 0)
+                    continue;
+
+                if (line.IndexOf("Global",StringComparison.InvariantCulture) == 0)
+                    break;
+
+                var slnRelativePath = line.Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries)[1].Trim().Replace("\"", string.Empty);
+                if (!slnRelativePath.EndsWith(".csproj"))
+                    continue;
+                
+                yield return Path.Combine(solutionDirectory, slnRelativePath);
+            }
+        }
+
+
+        private void HandleProjectFile(string projectFileLocation)
+        {
+            var project = new Project(projectFileLocation);
+            var license = project.GetItemsByEvaluatedInclude(@"Properties\licenses.licx").SingleOrDefault();
+            if (license != null)
+            {
+                project.RemoveItem(license);
+                project.Save();
+
+                foreach (var licx in Directory.EnumerateFiles(Path.GetDirectoryName(projectFileLocation) ?? string.Empty, "*.licx", SearchOption.AllDirectories))
+                {
+                    File.Delete(licx);
+                }
+            }
+
+        }
     }
 }
